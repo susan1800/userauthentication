c01d1cb5c7cd84a83af4c24479982c88d6c7ead9
susan timalsina <timalsinasusan14@gmail.com>
2022-06-07 18:37:54 +0545

    mail

--- resources/views/mail/sendotp.blade.php
+++ resources/views/mail/sendotp.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Localhost</title>
+    <title>Cosmos College</title>
 </head>
 <body>
     <h1>{{ $details['title'] }}</h1>
     <p>{{ $details['body'] }}<br><br><br></p>
     <h1>{{$details['head']}}</h1>
-   
+
     <p>Thank you</p>
 </body>
 </html>
